6a351631f7b24f41ed39cf43303fa1e0f9a79a6c
wibe <wibestn@gmail.com>
2018-09-23 14:50:04 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const deepFreeze = require("deep-freeze");
+const chai = require("chai");
+const expect = chai.expect;
+chai.use(require("chai-as-promised"));
+chai.use(require("chai-subset"));
+const sinon = require("sinon");
+chai.use(require("sinon-chai"));
+const Bottle = require("bottlejs");
+
+const statics = {
+  chai, expect, sinon, deepFreeze, Bottle,
+}
+
+function setup() {
+  return { ...statics, bottle: new Bottle };
+}
+
+module.exports = setup;
